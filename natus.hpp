d166be9d6e3da8d154d6556a2f1ec7411569a298
Lucca <julienlucca@gmail.com>
2021-02-01 18:05:59 -0300

    Improve docs and change some relations

--- natus.hpp
+++ natus.hpp
@@ -1,6 +1,30 @@
 #include <eosio/eosio.hpp>
 #include <eosio/asset.hpp>
 
+/**
+ * This contract describe the rules and behaviors for Natus Unit. 
+ * Natus Unit is a digital good that represents the services provided by nature within a PPA (Private Protected Area)
+ * 
+ * This is an effort to bring funds to those areas to help it grow an be preserved.
+ * With the funds obtained from the Natus Unit project, PPAs can study their land, discover what ecossystem services nature provide.
+ * That data allow measure of the production of those ecossystem services.
+ * Each year Natus Foundation measure that production and generate Natus Units, that comes from the PPAs, that measure is called `harvest`
+ * 
+ * # What can you do with it?
+ * 
+ * Like any digital good Natus Units can be exchanged between users.
+ * You can also choose to plant it. It will be in cultivation phase and will yield the planter a reward in the next harvest.
+ * Planted Natus U are cannot be exchanged anymore and represent a commitment with the land and nature represented by the Natus Unit
+ * 
+ * ### Links
+ * 
+ * https://en.wikipedia.org/wiki/Private_protected_area
+ * 
+ * #### Implementation of PPA in Brazil
+ * 
+ * https://en.wikipedia.org/wiki/Private_natural_heritage_reserve_(Brazil)
+ * https://pt.wikipedia.org/wiki/Reserva_particular_do_patrimônio_natural
+ */
 class [[eosio::contract("natus")]] natus : public eosio::contract
 {
 public:
@@ -8,72 +32,76 @@ public:
 
     /**
      * Table that holds information about all Natus Units
-     * Natus Unit is a digital good that represents a basket of services provided by a given RPPN (see links below) in a given period called `harvest`
-     * 
-     * What can you do with it?
-     * Like any digital good Natus Units can be exchanged between users.
-     * You can also choose to plant it. It will be in cultivation phase and will yield the planter a reward in the next harvest.
-     * Planted Natus U are cannot be exchanged anymore and represent a commitment with the land and nature represented by the Natus Unit
+     * Natus Unit is a digital good that represents a basket of services provided by a given PPA (see links below) in a given period called `harvest`
      * 
      * Here is a general description of available fields on a Natus Unit:
      * 
      * * id: ID
      * * owner: Current owner 
-     * * origin: RPPN ID
+     * * origin: PPA ID
      * * harvest: Period of harvest, usually happens on 365 days cycle but not always
      * * report_hash: Hash for the full report document, used to garantee authenticity of the document
-     * * biome: RPPN's biome
-     * * location: RPPN's location using latitude and longitude
-     * * planted_date: Date it was planted, its empty if it is still not planted
-     * 
-     * https://en.wikipedia.org/wiki/Private_natural_heritage_reserve_(Brazil)
-     * https://pt.wikipedia.org/wiki/Reserva_particular_do_patrimônio_natural
+     * * planted_at: Date it was planted, its empty if it is still not planted
+     * * inserted_at: Date it was inserted/created
+     * * updated_at: Date it was updated
      */
     TABLE units
     {
         std::uint32_t id;
         eosio::name owner;
         std::string origin;
-        // std::uint64_t harvest;
         std::string harvest;
         std::string report_hash;
-        std::string biome;
-        std::string location;
 
-        std::uint32_t planted_date;
+        std::uint32_t planted_at;
+        std::uint64_t inserted_at;
+        std::uint64_t updated_at;
 
         std::uint32_t primary_key() const { return id; }
 
-        EOSLIB_SERIALIZE(units, (id)(owner)(origin)(harvest)(report_hash)(biome)(location)(planted_date));
+        EOSLIB_SERIALIZE(units, (id)(owner)(origin)(harvest)(report_hash)(planted_at)(inserted_at)(updated_at));
     };
 
-    // TODO: How should we include that? On issue directly?
-    TABLE services
+    TABLE ecoservices
     {
         std::uint32_t id;
-        std::uint32_t unit_id;
+        std::uint32_t ppa_id;
+        std::uint32_t harvest_id;
         std::string category;
         std::string subcategory;
         std::float value;
 
         std::uint32_t primary_key() const { return id; }
 
-        EOSLIB_SERIALIZE(services, (id)(unit_id)(category)(subcategory)(value));
+        EOSLIB_SERIALIZE(ecoservices, (id)(ppa_id)(harvest_id)(category)(subcategory)(value));
     };
 
-    TABLE harvest{};
+    TABLE harvest
+    {
+        std::uint32_t id;
+        std::uint32_t year;
+        std::string name; // Eg.: "2021.1", "2021.2"
 
-    TABLE rppn
+        std::uint32_t primary_key() const { return id; }
+
+        EOSLIB_SERIALIZE(harvest, (id)(year)(name));
+    };
+
+    /**
+     * Central data structure, represents a PPA
+    */
+    TABLE ppa
     {
         std::uint32_t id;
         eosio::name owner;
         std::string name;
         std::string biome;
         std::string location;
+        std::string country; // TODO: Check if there is an international country table
 
         std::uint32_t primary_key() const { return id; }
 
-        EOSLIB_SERIALIZE(rppn, (id)(owner)(name)(location)(biome));
+        EOSLIB_SERIALIZE(ppa, (id)(owner)(name)(biome)(location)(country));
     };
 
     /**
@@ -81,13 +109,13 @@ public:
      * 
      * Validations:
      * * Owner: Owner EOSIO Account, must be valid and exist
-     * * Origin: Origin RPPN ID, must be present in the RPPN table
+     * * Origin: Origin PPA ID, must be present in the PPA table
      * * Harvest: Harvest ID, must be present on the Havest table
      * * Report Hash: must be 256 chars long using MD-5
-     * * Biome: Must be one of the following: `pantanal`, `atlanticflorest`, `amazonrainflorest`
-     * * Location: Make sure location is on the right format. Must be 0.000000-0.000000
      */
-    ACTION issue(eosio::name to, eosio::name owner, std::string origin, std::string harvest, std::string report_hash std::string biome, std::string location);
+    ACTION issue(eosio::name to, eosio::name owner, std::string origin,
+                 std::string harvest, std::string report_hash std::string biome,
+                 std::string location);
 
     /**
      * Plant a Natus Unit
@@ -100,34 +128,51 @@ public:
     ACTION plant(std::uint32_t id, eosio::name owner, std::string date);
 
     /**
-     * Upsert RPPN 
-     * Admin only action that insert or update an RPPN
+     * Upsert PPA 
+     * Admin only action that insert or update an PPA
+     * 
+     * Update only allows changes on `name` and `owner`
      * 
      * Validations:
      * * Id: ID of an existing service, send it as 0 to insert a new entry
-     * * Owner: Owner account for the RPPN
-     * * Name: Name for the RPPN, limited to 255 characters
-     * * Biome: Must be one of the following: `pantanal`, `atlanticflorest`, `amazonrainflorest`
-     * * Location: Make sure location is on the right format. Must be 0.000000-0.000000
+     * * Owner: Owner account for the PPA, can be updated
+     * * Name: Name for the PPA, limited to 255 characters, can be updated
+     * * Biome: Must be one of the following: `pantanal`, `atlanticflorest`, `amazonrainflorest`, `caatinga`, cannot be updated
+     * * Location: Make sure location is on the right format. Must be 0.000000-0.000000, cannot be updated
+     * * Country: Country code where the PPA is located, cannot be updated
      */
-    ACTION upsertrppn(std::uint32_t id, eosio::name owner, std::string name, std::string biome, std::string location);
+    ACTION upsertppa(std::uint32_t id, eosio::name owner, std::string name, std::string biome,
+                     std::string location, std::string country);
 
     /**
-     * Upser services
+     * Upsert ecoservices
      * 
      * It is an admin only action that insert or update an service. As Natus evolves new ecosystem services will be added
-     * Natus Units can have a basket different services with different values. 
+     * Natus Units can have a basket different services with different values that came from the PPA. 
+     * 
+     * Services are added at each new harvest and cannot be changed after.
+     * 
+     * Keys are `PPA_id` and `harvest`
      * 
      * Validations:
      * * Id: ID of an existing service, send it as 0 to insert a new entry
-     * * Unit ID: Natus Unit ID. Must exist
+     * * PPA ID: PPA ID. Must exist
+     * * Harvest ID: ID of an existing harvest
      * * Category: must be `water`, `biodiversity` or `carbon`
      * * Sub category: 
-     * *    When category is `water`: `course` or `sprint`.
-     * *    When category is `carbon`: `capture`. 
+     * *    When category is `water`: `course` or `spring`.
+     * *    When category is `carbon`: `stock`. 
      * *    When category is `biodiversity`: `vegetation`, `species`, `hotspot`
      * * Value: float value corresponding to the service provided
+     * 
+     * ID   PPA_ID      Harvest     Category     Subcategory     Value
+     * 1    gigante1    2020.1      water        spring          10
+     * 2    aranhas1    2020.1      water        course          3000
+     * 3    gigante1    2021.1      carbon       stock           50
      */
-    // TODO: Should we allow update on a Natus Unit services? does it makes sense?
-    ACTION upsertservices(std::uint32_t id, std::uint32_t unit_id, std::string category, std::string subcategory, std::float value);
+    ACTION upsertservices(std::uint32_t id, std::uint32_t ppa_id, std::uint32_t harvest_id, std::string category,
+                          std::string subcategory, std::float value);
+
+    // TODO: Create transfer
+    // Transfer must be blocked if is planted
 }
